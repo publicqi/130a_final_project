da307a149ffa657e38c6b4d9e9b66a8abe62540c
publicqi <qisu@ucsb.edu>
2021-05-27 21:15:53 +0800

    RBTree structure

---
+++ RBTree.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#define RED 0
+#define BLACK 1
+
+using namespace std;
+
+class RBTree{
+private:
+    class Node{
+    public:
+        Node(string data);
+        string data;
+        Node *left = nullptr;
+        Node *right = nullptr;
+        Node *parent = nullptr;
+        int color = RED;
+    };
+
+    Node* root = nullptr;
+
+    void rotateLeft(Node *&, Node *&);
+    void rotateRight(Node *&, Node *&);
+    void fixViolation(Node *&, Node *&);
+
+public:
+    ~RBTree();
+    void insertValue(string value);
+    Node* searchValue(string value);
+    vector<Node*> rangeSearch(string value1, string value2);
+};
